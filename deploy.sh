c206d64e3d0a89a84832ad94bd9cb63f45225c68
Manuel Mejia <manuel@buildinglink.com>
2019-10-06 20:46:00 -0400

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# abort on errors
+set -e
+
+# build
+echo Linting..
+npm run lint
+echo Building. this may take a minute...
+npm run build
+
+# if you are deploying to a custom domain add a CNAME (uncomment the next 3 lines)
+#cd dist
+#echo 'yourcustomdomain.com' > CNAME
+#cd -
+
+# deploy
+echo Deploying..
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/mejiamanuel57/gh-pages-vue-cli-3.git master
