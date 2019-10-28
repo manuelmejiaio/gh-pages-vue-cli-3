7a6e86f87f7bb6621736fe8f2fa4cdea165a8611
Manuel Mejia <manuel@buildinglink.com>
2019-10-28 17:50:26 -0400

    updating vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/gh-pages-vue-cli-3/' : '/'
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') return
+
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'docs'),
+          routes: ['/', '/about'],
+          minify: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            keepClosingSlash: true,
+            sortAttributes: true
+          },
+          renderer: new Renderer({ headless: true })
+        })
+      ]
+    }
+  }
 }
